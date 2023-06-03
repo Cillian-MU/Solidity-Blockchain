fc133784e4936fc883be2ab4e19fca13590f32eb
EIRE <72869475+Cillian-MU@users.noreply.github.com>
2023-06-03 20:26:10 +0000

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -6,15 +6,20 @@ contract ERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    string constant public name = "MyFirstToken";
-    string constant public symbol = "MFT";
-    uint8 constant public decimals = 18;
-
+    string public name;
+    string public symbol;
+    uint8 immutable public decimals;
     uint256 public totalSupply;
 
     mapping (address => uint256) public balanceOf;
 
     mapping (address => mapping (address => uint256)) public allowance;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+    }
 
     function transfer(address to, uint256 value) external returns (bool) {
         return _transfer(msg.sender, to, value);
