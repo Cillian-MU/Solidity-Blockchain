b6413dd22b995e0f443655612e8217172add1d9b
EIRE <72869475+Cillian-MU@users.noreply.github.com>
2023-06-04 12:04:46 +0000

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,67 +1,58 @@
-// SPDX-Licence-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-contract ERC20 {
+import { IERC20 } from "./IERC20.sol";
+import {console} from "hardhat/console.sol";
+
+contract ERC20 is IERC20{
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
     string public name;
     string public symbol;
-    uint8 immutable public decimals;
+    uint8 public immutable decimals;
     uint256 public totalSupply;
 
     address public owner;
 
-    mapping (address => uint256) public balanceOf;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    mapping (address => mapping (address => uint256)) public allowance;
-
-    constructor(address _owner, string memory _name, string memory _symbol, uint8 _decimals) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-
-        owner = _owner;
     }
 
     function transfer(address to, uint256 value) external returns (bool) {
         return _transfer(msg.sender, to, value);
     }
 
-    function _mint(address to, uint256 value) private {
+    function _mint(address to, uint256 value) internal {
         balanceOf[to] += value;
         totalSupply += value;
 
         emit Transfer(address(0), to, value);
     }
 
-    function mint(address to, uint256 value) external onlyOwner {
-
-        _mint(to, value);
-    }
-
-    //burns token from a address and removes it from supply
-    function _burn(address from, uint256 value) private {
+    function _burn(address from, uint256 value) internal {
         balanceOf[from] -= value;
         totalSupply -= value;
 
         emit Transfer(from, address(0), value);
     }
 
-    function burn(address from, uint256 value) external onlyOwner {
-        _burn(from, value);
-    }
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool) {
+        require(
+            allowance[from][msg.sender] >= value,
+            "ERC20: Insufficient allowance"
+        );
 
-    function transferFrom(address from, address to, uint256 value) external returns (bool) {
-        require(allowance[from][msg.sender] >= value, "ERC20: Insufficent allowance");
-       
-        //check if sender is allowed to use the funds from the from address
         allowance[from][msg.sender] -= value;
 
         emit Approval(from, msg.sender, allowance[from][msg.sender]);
@@ -69,8 +60,12 @@ contract ERC20 {
         return _transfer(from, to, value);
     }
 
-    function _transfer(address from, address to, uint256 value) private returns (bool) {
-        require(balanceOf[from] >= value, "ERC20: Insufficent sender balance");
+    function _transfer(
+        address from,
+        address to,
+        uint256 value
+    ) private returns (bool) {
+        require(balanceOf[from] >= value, "ERC20: Insufficient sender balance");
 
         emit Transfer(from, to, value);
 
@@ -80,8 +75,10 @@ contract ERC20 {
         return true;
     }
 
-    function approve(address spender, uint256 value) external returns (bool) {
-
+    function approve(
+        address spender,
+        uint256 value
+    ) external payable returns (bool) {
         allowance[msg.sender][spender] += value;
 
         emit Approval(msg.sender, spender, value);
